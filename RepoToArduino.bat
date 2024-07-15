94078eb269778d0ed0bc3bef2f2e4480bc7084d0
Martin Emch <memch@gmx.ch>
2024-07-15 23:05:09 +0200

    RepoToArduino.bat

--- RepoToArduino.bat
+++ RepoToArduino.bat
@@ -8,7 +8,6 @@ echo WinMergeUExe=%WinMergeUExe%
 :: https://manual.winmerge.org/en/Command_line.html
 "C:\Program Files\WinMerge\WinMergeU.exe" /r /f * /wr "%CD%" "%USERPROFILE%\Documents\Arduino"
 
-
 :END
 echo --------------------------------------------------------------------------------
 echo %DATE% %TIME% %~nx0 for user: "%USERNAME%" on computer: "%COMPUTERNAME%" ended
